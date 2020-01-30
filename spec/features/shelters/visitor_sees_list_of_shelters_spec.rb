5ba0317959d1dcbcc860bcf74f2d93e7d9b4adad
Cassie Achzenick <caachzenick@gmail.com>
2020-01-29 19:59:40 -0700

    Add user story 4 and passing tests.

--- spec/features/shelters/visitor_sees_list_of_shelters_spec.rb
+++ spec/features/shelters/visitor_sees_list_of_shelters_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 RSpec.describe "shelters index page", type: :feature do
   it "shows all shelters" do
-    shelter1 = Shelter.create!(name: 'humane society')
-    shelter2 = Shelter.create(name: "Pound")
-    shleter3 = Shelter.create(name: "Old Dogs Home")
+    shelter1 = Shelter.create(name: 'humane society', address: "1234 st", city: 'Denver', state: 'Colorado', zip: 29572)
+    shelter2 = Shelter.create(name: "Pound", address: "5678 st", city: 'Seattle', state: 'Washington', zip: 390507)
+    shleter3 = Shelter.create(name: "Old Dogs Home", address: '2948 st', city: 'San Diego', state: 'California', zip: 30958)
 
     visit "/shelters"
 
