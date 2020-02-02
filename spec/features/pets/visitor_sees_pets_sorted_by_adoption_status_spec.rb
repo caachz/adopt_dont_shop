4a05c385698a2ec2642b19794a2c72c08f1a68df
Cassie Achzenick <caachzenick@gmail.com>
2020-02-02 16:58:39 -0700

    Add spec harness

--- spec/features/pets/visitor_sees_pets_sorted_by_adoption_status_spec.rb
+++ spec/features/pets/visitor_sees_pets_sorted_by_adoption_status_spec.rb
@@ -1,15 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "A visitor can see the total pet count at the top of every shelter pets index page" do
-  it "gets a total pet count" do
+RSpec.describe "A visitor can see pets sorted by adoption status in the shelter pets index page" do
+  it "sorts pets by adoption status" do
     shelter1 = Shelter.create!(name: 'humane society', address: "1234 st", city: 'Denver', state: 'Colorado', zip: "29572")
 
-    snickers = Pet.create!(image: 'https://images-na.ssl-images-amazon.com/images/I/41Q-6cQEOLL._AC_SY400_.jpg', name: 'Snickers', age: 15, sex: 'Female', shelter: shelter1)
+    snickers = Pet.create!(image: 'https://images-na.ssl-images-amazon.com/images/I/41Q-6cQEOLL._AC_SY400_.jpg', name: 'Snickers', age: 15, sex: 'Female', shelter: shelter1, adoption_status: "pending adoption")
     abbey = Pet.create!(image: 'https://www.rd.com/wp-content/uploads/2019/09/01_Doggie-dino-760x506.jpg', name: 'Abbey', age: 4, sex: 'Female', shelter: shelter1)
     sadie = Pet.create!(image: 'https://images.halloweencostumes.com/products/45834/1-1/dog-dino-pup-costume.jpg', name: 'Sadie', age: 3, sex: "Female", shelter: shelter1)
-
-    visit "shelters/#{shelter1.id}/pets"
-
-    page.body.should =~ /abbey.*sadie.*snickers/
   end
 end
